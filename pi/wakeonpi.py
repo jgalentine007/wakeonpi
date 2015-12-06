50c1a1f25711d158ef7e0f0822011cbb4f9a6f56
Jonathan Galentine <j.galentine@gmail.com>
2015-12-06 15:48:17 -0500

    Getting started
    Sample  YAML configuration file.  Argument processing and loading
    configuration file.

---
+++ pi/wakeonpi.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# wakeonpi.py
+
+import socket
+import struct
+import yaml
+import tweepy
+import sys, getopt
+
+def wake_on_lan(macaddress):
+    """ Switches on remote computers using WOL. """
+
+    # Check macaddress format and try to compensate.
+    if len(macaddress) == 12:
+        pass
+    elif len(macaddress) == 12 + 5:
+        sep = macaddress[2]
+        macaddress = macaddress.replace(sep, '')
+    else:
+        raise ValueError('Incorrect MAC address format')
+ 
+    # Pad the synchronization stream.
+    data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
+    send_data = '' 
+
+    # Split up the hex values and pack.
+    for i in range(0, len(data), 2):
+        send_data = ''.join([send_data,
+                             struct.pack('B', int(data[i: i + 2], 16))])
+
+    # Broadcast it to the LAN.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(send_data, ('<broadcast>', 7))
+    
+def usage():
+	print "wakeonpi.py -c <confFile>"
+	sys.exit(2)
+	
+def main(argv):
+	# Use macaddresses with any seperators.
+    #wake_on_lan('0F:0F:DF:0F:BF:EF')
+    #wake_on_lan('0F-0F-DF-0F-BF-EF')
+    # or without any seperators.
+    #wake_on_lan('0F0FDF0FBFEF')
+	
+	confFile = ''
+	confData = None
+	
+	# read command line arguments
+	if len(argv) == 0: 
+		usage()
+		
+	try:
+		opts, args = getopt.getopt(argv, "c:", ["conf="])
+	except getopt.GetoptError:
+		usage()
+		
+	for opt, arg in opts:
+		if opt in ("-c", "--conf"):
+			confFile = arg
+		
+		
+	# read and parse configuration file
+	try:
+		stream = open(confFile, "r")
+		confData = yaml.load(stream)
+		#stream.close()
+	except IOError:
+		print "Error reading configuration file."
+		sys.exit(2)
+	except:
+		print "Error parsing YAML in configuration file."
+		sys.exit(2)
+	
+	print confData
+	
+	consumer_key = confData["twitter"]["consumer_key"]
+	consumer_secret = confData["twitter"]["consumer_secret"]
+	access_token = confData["twitter"]["access_token"]
+	access_token_secret = confData["twitter"]["access_token_secret"]	
+	computers = confData["computers"]
+	
+	# print "consumer_key: ", consumer_key
+	# print "consumer_secret: ", consumer_secret
+	# print "access_token: ", access_token
+	# print "access_token_secret: ", access_token_secret
+	# print "computers: ", computers
+	
+
+if __name__ == '__main__':
+	
+	main(sys.argv[1:])
